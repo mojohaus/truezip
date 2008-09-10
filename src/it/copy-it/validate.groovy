bf6a91c8c467555968e452662a45f37e5f3b137b
Dan Tran <dantran@gmail.com>
2008-09-10 03:47:51 +0000

    copy-it validation with groovy
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/sandbox/truezip-maven-plugin@7626 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/copy-it/validate.groovy
+++ src/it/copy-it/validate.groovy
@@ -3,3 +3,11 @@ assert new File(basedir, 'target/copy-file/pom.with.xml.extension').exists();
 
 assert new File(basedir, 'target/copy-fileset/META-INF/MANIFEST.MF').exists();
 assert new File(basedir, 'target/copy-fileset/META-INF/maven/org.codehaus.mojo/truezip-maven-plugin-test/pom.xml').exists();
+
+
+list = new File(basedir,'target/copy-into.list').text;
+
+assert list.contains( 'build.log' )
+assert list.contains( 'pom.xml' )
+assert list.contains( 'goals.txt' )
+assert ! list.contains( 'validate.groovy' )
