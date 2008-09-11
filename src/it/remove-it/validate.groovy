810d02a5d7c2f7570b472ac65f00845d78f1cb22
Dan Tran <dantran@gmail.com>
2008-09-11 05:14:45 +0000

    better IT validation with groovy regular expresstio
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/sandbox/truezip-maven-plugin@7651 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/remove-it/validate.groovy
+++ src/it/remove-it/validate.groovy
@@ -1,11 +1,15 @@
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
 assert ! new File(basedir, 'target/truezip-maven-plugin-test.jar').exists();
 
 list = new File(basedir,'target/remove-a-file.list').text;
 assert ! list.contains( 'pom.properties' )
-assert list.contains( 'maven' )
+assert   list =~ /.*META-INF.maven.org.codehaus.mojo.truezip-maven-plugin-test.pom.xml.*/
+assert   list =~ /.*META-INF.MANIFEST.MF.*/
 
 list = new File(basedir,'target/remove-a-directory.list').text;
 assert ! list.contains( 'maven' )
-assert list.contains( 'MANIFEST.MF' )
+assert   list =~ /.*META-INF.MANIFEST.MF.*/
 
-    
+
