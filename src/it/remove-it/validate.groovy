2e10a523f118f45af4b5cca8aa095c3da8888812
Dan Tran <dantran@gmail.com>
2009-01-15 05:19:41 +0000

    all IT tests now run with java4
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/sandbox/truezip-maven-plugin@8677 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/remove-it/validate.groovy
+++ src/it/remove-it/validate.groovy
@@ -5,11 +5,11 @@ assert ! new File(basedir, 'target/truezip-maven-plugin-test.jar').exists();
 
 list = new File(basedir,'target/remove-a-file.list').text;
 assert ! list.contains( 'pom.properties' )
-assert   list =~ /.*META-INF.maven.org.codehaus.mojo.truezip-maven-plugin-test.pom.xml.*/
+assert   list =~ /.*META-INF.maven.org.codehaus.mojo.truezip.it.truezip-maven-plugin-test.pom.xml.*/
 assert   list =~ /.*META-INF.MANIFEST.MF.*/
 
 list = new File(basedir,'target/remove-a-directory.list').text;
-assert ! list.contains( 'maven' )
+assert  list.contains( 'maven' )
 assert   list =~ /.*META-INF.MANIFEST.MF.*/
 
 
